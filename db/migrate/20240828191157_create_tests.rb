b008bb3afcfb5bd458681dead0a0e4f9b4af6377
TimDelRey <kepaliga@mail.ru>
2024-08-30 21:44:11 +0300

    fix

--- db/migrate/20240828191157_create_tests.rb
+++ db/migrate/20240828191157_create_tests.rb
@@ -1,9 +1,9 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title
+      t.string :title, null: false
       t.integer :level, default: 0
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: {to_table: :users}
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
