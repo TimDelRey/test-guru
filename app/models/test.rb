b627256c7f458e84c5f870d8f7d6c6de53c3e636
TimDelRey <kepaliga@mail.ru>
2024-09-06 22:04:15 +0300

    feat, added class method search tests by category:title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+
+  belongs_to :category
+
+  def self.desc_tests_by_category (name_category)
+    necessary_part_of_tests = Test.select(:id, :title, :updated_at).order(updated_at: :desc)
+    necessary_part_of_tests.joins(:category).where(category: { title: name_category }).pluck(:title)
+  end
 end
