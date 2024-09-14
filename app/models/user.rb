832ed0582320f6280e124f1d469547f8c2c07a25
TimDelRey <kepaliga@mail.ru>
2024-09-14 22:21:48 +0300

    feat, созданы ассоциации между моделями

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+  has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
+  has_many :started_test
+  has_many :tests, through: :started_test
+
   def users_history_by_level(level)
     Test.joins('JOIN started_tests ON started_tests.test_id = tests.id')
       .where(started_tests: { user_id: id }, tests: { level: level })
