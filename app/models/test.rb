83c030c7c4fed468a3e51d24451c6688e90bce98
TimDelRey <kepaliga@mail.ru>
2024-09-17 19:47:15 +0300

    fix, написаны методы создающие строки соединительной таблицы в seeds, откорректирован метод users_history_by_level в соответствии с ассоциациями, уточнены ассоциации моделей при помощи dependent

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,17 @@
 class Test < ApplicationRecord
 
   belongs_to :category
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :questions
-  has_many :started_tests
-  has_many :users, through: :started_tests
+  belongs_to :author, 
+              class_name: 'User', 
+              foreign_key: 'user_id'
+  has_many :questions, 
+            dependent: :nullify
+  has_many :started_tests,
+            class_name: 'StartedTest',
+            foreign_key: 'test_id',
+            dependent: :destroy
+  has_many :users, through: :started_tests, 
+            dependent: :nullify
 
   def self.desc_tests_by_category (name_category)
     select(:title, :updated_at)
